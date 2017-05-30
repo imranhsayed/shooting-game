049f7d1262d06754ebcbc7c4ff298ea60dca65b2
gsayed786 <Ghafir.Sayed@gmail.com>
2017-05-30 05:32:03 +0530

    completed level 3

--- javascript.js
+++ javascript.js
@@ -17,6 +17,7 @@ var birdAction = {
     backgroundMusicLevel3: null,
     thunderstormMusic: null,
     angryDragonScream:null,
+    dragonImageEl: null,
     gunImage: null,
     gunImageSrc: 'images/manshooting.png',
     birdImageLeft: null,
@@ -37,7 +38,7 @@ var birdAction = {
     birdNoToSend: 0,
     randRange: 10,
     birdSpeed: 10000,
-    health: null,
+    health: 200,
 
     init: function () {
 
@@ -135,14 +136,7 @@ var birdAction = {
 
 
         document.querySelector( 'section' ).classList.remove( 'display' );
-        if( $( 'section').hasClass( 'rain') ){
-            document.querySelector( 'section' ).classList.remove( 'rain' );
-        }
-        if( 0 != $( 'section' ).find( 'div.drop').length ){
-            for( var i = 1; i < birdAction.nbRainDrop; i++){
-                document.getElementById( 'drop' + i  ).remove();
-            }
-        }
+        birdAction.stopRain();
         document.querySelector( '.game-over').classList.add( 'display');
 
 
@@ -154,8 +148,8 @@ var birdAction = {
     },
 
     startLevelThree: function () {
-        birdAction.gameTimer = 25;
-        if ( false === $( '.left-bird-nest' ).hasClass( 'display' )   ){
+        birdAction.gameTimer = 120;
+        if ( true === $( '.content div').first( ).hasClass( 'left-bird-nest' ) && false === $( '.left-bird-nest' ).hasClass( 'display' )   ){
             document.querySelector( '.left-bird-nest' ).remove();
             document.querySelector( '.right-bird-nest' ).remove();
 
@@ -185,7 +179,9 @@ var birdAction = {
         var lightening = 0;
         var shortLightening = setInterval( function () {
             lightening = lightening + 1;
-            console.log( 'lightening' , lightening );
+            /**
+             *  Till 10 secs
+             */
             if ( lightening > 10 ){
                 body.style.backgroundImage = "url( 'images/dark-castle.gif' )";
                 /**
@@ -197,19 +193,27 @@ var birdAction = {
                 }, false);
                 birdAction.backgroundMusicLevel3.play();
                 document.querySelector( '.gun-image' ).classList.remove( 'display');
-                $( '<p></p>', {
-                    class: 'dragon-health-name',
-                    text: 'Dragon Health Bar'
-                }).appendTo( '.header' );
-                $( '<progress></progress>', {
-                    id: 'health',
-                    value: '200',
-                    max:'400',
-                }).appendTo( '.header' );
+                if( true === $( '.header' ).hasClass( 'display' ) ){
+                    document.querySelector( '.header' ).classList.remove( 'display' );
+                }
+
+                    $( '<p></p>', {
+                        class: 'dragon-health-name',
+                        text: 'Dragon Health Bar'
+                    }).appendTo( '.header' );
+                    $( '<progress></progress>', {
+                        id: 'health',
+                        class: 'progress-class',
+                        value: '200',
+                        max:'400',
+                    }).appendTo( '.header' );
+
+                birdAction.health = document.getElementById( "health" );
                 document.querySelector( '.time-remaining' ).classList.remove( 'display' );
                 var dragonEntryGreen = document.querySelector( '.dragon-entry-green' );
-                dragonEntryGreen.addEventListener( 'click', birdAction.dragonHitGreen )
-
+                birdAction.dragonImageEl = dragonEntryGreen;
+                dragonEntryGreen.addEventListener( 'click', birdAction.dragonHitStronger )
+                birdAction.newDragonEntry();
                 clearInterval( shortLightening );
                 return;
             }
@@ -235,17 +239,17 @@ var birdAction = {
 
         },1000);
 
-        if ( true === $( 'section' ).hasClass( 'display')   ){
-            document.querySelector( 'section' ).classList.add( 'rain');
-            document.querySelector( 'section' ).classList.remove( 'display');
-
-            birdAction.createRain();
-        }
         if ( false === $( '.game-over' ).hasClass( 'display')   ){
                 document.querySelector( '.game-over' ).classList.add( 'display');
         }
 
         birdAction.gameTimerLevel3();
+        if ( true === $( 'section' ).hasClass( 'display')   ){
+            document.querySelector( 'section' ).classList.remove( 'display');
+        }
+
+        document.querySelector( 'section' ).classList.add( 'rain');
+        birdAction.createRain();
 
     },
 
@@ -515,6 +519,18 @@ var birdAction = {
             $('#drop'+i).css('top',dropTop);
         }
     },
+
+    stopRain: function () {
+        if( $( 'section').hasClass( 'rain') ){
+            document.querySelector( 'section' ).classList.remove( 'rain' );
+        }
+        if( 0 != $( 'section' ).find( 'div.drop').length ){
+            for( var i = 1; i < birdAction.nbRainDrop; i++){
+                document.getElementById( 'drop' + i  ).remove();
+            }
+        }
+    },
+
     gameTimerLevel1: function ( ) {
          birdAction.t = birdAction.gameTimer;
 
@@ -599,7 +615,7 @@ var birdAction = {
                 return;
             }
 
-            if( ( birdAction.health.value ===  0 ) && ( 0 < birdAction.t ) )  {
+            if( ( 0 ===  birdAction.health.value ) && ( 0 < birdAction.t ) )  {
                 birdAction.gameOverLevel3( birdAction.t );
                 clearInterval( scoreIntervalCheck );
             }
@@ -708,24 +724,42 @@ var birdAction = {
     },
     gameOverLevel3: function () {
         birdAction.angryDragonScream.pause();
-        document.querySelector( '.dragon-entry-green' ).remove();
-        document.querySelector( 'section' ).classList.remove( 'rain' );
+        if( $( '.content' ).children().hasClass( 'dragon-entry-green' ) ){
+            document.querySelector( '.dragon-entry-green' ).remove();
+        }
+        birdAction.stopRain();
+        document.querySelector( '.header' ).classList.add( 'display' );
+
         var timeTookToFinish = birdAction.gameTimer - birdAction.t,
-            gameOverDiv = document.querySelector( '.game-over');
-
-
+            gameOverDiv = document.querySelector( '.game-over'),
+            gameOverScore = document.querySelector( '.game-over-score');
 
             if( ( birdAction.health.value ===  0 ) && ( 0 < birdAction.t ) )  {
 
             $( '<p></p>', {
-                text: 'Congratulations You have completed all three levels.You Killed the Dragon in '+ timeTookToFinish + ' secs',
+                text: 'Congratulations You have completed all three levels.You Killed the Dragon in '
+                        + timeTookToFinish + ' secs. ' +' Go back to the home Screen and Restart the Game',
                 class: 'congrats-text'
             } ).prependTo( gameOverDiv );
-            document.querySelector( '.level2-start-button' ).classList.add( 'display' );
-            document.querySelector( '.game-over').classList.remove( 'display' );
-            document.querySelector( '.level3-start-button' ).classList.remove( 'display' );
-            $( 'button.level3-start-button' ).on( 'click', birdAction.startLevelThree );
-        }
+                document.querySelector( '.game-over-score' ).textContent = "";
+                $( '.level3-start-button' ).replaceWith( '<button class="level2-start-button">Go to Home Screen</button>' );
+            $( 'button.level2-start-button' ).on( 'click', birdAction.gameRestartLevel1 );
+        }else{
+
+                var couldNotScoreEl = document.createElement( 'p' ),
+                    couldNotScoreText = document.createTextNode( 'Sorry you could not kill the Dragon in Required Time. Please Try Again' );
+                couldNotScoreEl.appendChild( couldNotScoreText );
+                document.querySelector( '.game-over-score' ).textContent = "";
+                gameOverScore.appendChild( couldNotScoreEl );
+                document.querySelector( '.level2-start-button' ).classList.add( 'display' );
+                document.querySelector( '.level3-start-button' ).classList.remove( 'display' );
+                document.querySelector( 'progress' ).remove();
+                document.querySelector( '.dragon-health-name' ).remove();
+
+                // $( 'button.level3-start-button' ).on( 'click', birdAction.startLevelThree );
+            }
+        document.querySelector( '.game-over').classList.remove( 'display' );
+
 
 
 
@@ -749,22 +783,186 @@ var birdAction = {
         document.querySelector( '.game-over').classList.add( 'display');
         birdAction.startLevelTwo();
     },
-    dragonHitGreen: function () {
+    newDragonEntry: function () {
+
+        var timeInterval = 0;
+        var newDragonEntryInterval = setInterval( function () {
+            timeInterval += 1;
+            console.log( 'Main timeInterval = ', timeInterval );
+
+            /**
+             * After 15 secs from the start of Level3 Game
+             */
+            if ( timeInterval > 5 ){
+                $( '.dragon-entry-green' ).fadeOut( 2000 );
+            }
+            if ( timeInterval > 10 ){
+                if( false === $( '.content').children().hasClass( 'dragon-entry-brown') ){
+                    console.log( 'Dragon-entry-brown at 11 sec = ', timeInterval );
+                    $( '.dragon-entry-green' ).remove();
+                    $( '<img></img>', {
+                        src: 'images/dragon-entry-brown.png',
+                        class: 'dragon-entry-brown'
+
+                    } ).fadeIn(1000)
+                        .delay( 2000 )
+                        .prependTo( '.content' )
+                        .animate(
+                            {
+                                'position': 'absolute',
+                                'bottom': '400px',
+                                'right': '1500px'
+                            }, 9000
+                        ).fadeOut( 5000 );
+                    birdAction.dragonImageEl = document.querySelector( '.dragon-entry-brown');
+                    birdAction.dragonImageEl.addEventListener( 'click', birdAction.dragonHitStronger );
+                }
+            }
+            if ( timeInterval > 21 ){
+                console.log( 'Dragon-entry-dark-green at 22 sec = ', timeInterval );
+                $( '.dragon-entry-brown' ).remove();
+                if( false === $( '.content').children().hasClass( 'dragon-entry-dark-green') ){
+                    $( '<img></img>', {
+                        src: 'images/dragon-entry-dark-green.gif',
+                        class: 'dragon-entry-dark-green'
+
+                    } ).fadeIn(1000)
+                        .delay( 2000 )
+                        .prependTo( '.content' )
+                        .animate(
+                            {
+                                'position': 'absolute',
+                                'left': '-300px',
+                                'bottom': '1500px'
+                            }, 10000
+                        )
+                        .fadeOut( 9000 );
+                    birdAction.dragonImageEl = document.querySelector( '.dragon-entry-dark-green');
+                    birdAction.dragonImageEl.addEventListener( 'click', birdAction.dragonHitStronger );
+                }
+
+            }
+            if( timeInterval > 32 ){
+                console.log( 'Dragon-fire-left at 33 sec = ', timeInterval );
+                $( '.dragon-entry-dark-green' ).remove();
+                if( false === $( '.content').children().hasClass( 'dragon-fire-left') ){
+                    console.log( 'timeInterval dragon-fire-left = ', timeInterval );
+                    $( '<img></img>', {
+                        src: 'images/dragon-fire-left.gif',
+                        class: 'dragon-fire-left'
+
+                    } ).fadeIn(1000)
+                        .prependTo( '.content' )
+                        .animate(
+                            {
+                                'position': 'absolute',
+                                'top': '-240px',
+                                'left': '-350px'
+                            }, 2000
+                        )
+                        .fadeOut( 5000 );
+                }
+            }
+            if( timeInterval > 40 ){
+                console.log( 'Dragon-fire-right at 41 sec = ', timeInterval );
+                $( '.dragon-fire-left' ).remove();
+                if( false === $( '.content').children().hasClass( 'dragon-fire-right') ){
+                    $( '<img></img>', {
+                        src: 'images/dragon-fire-right.gif',
+                        class: 'dragon-fire-right'
+
+                    } ).fadeIn(1000)
+                        .prependTo( '.content' )
+                        .animate(
+                            {
+                                'position': 'absolute',
+                                'top': '-160px',
+                                'left': '-170px'
+                            }, 2000
+                        )
+                        .fadeOut( 5000 );
+                }
+
+            }
+
+            if( timeInterval > 47 ){
+                console.log( 'Dragon-fire-full at 48 sec = ', timeInterval );
+
+                $( '.dragon-fire-right' ).remove();
+                if( false === $( '.content').children().hasClass( 'dragon-fire') ){
+                    $( '<img></img>', {
+                        src: 'images/dragon-fire.gif',
+                        class: 'dragon-fire'
+
+                    } ).fadeIn(1000)
+                        .prependTo( '.content' )
+                        .animate(
+                            {
+                                'position': 'absolute',
+                                'top': '400px',
+                                'right': '200px'
+                            }, 5000
+                        )
+                        .fadeOut( 4000 );
+                }
+            }
+            /**
+             * Dragon Red Vulnerable
+             */
+            if( timeInterval > 55 ){
+                console.log( 'Dragon-fire-full at 56 sec = ', timeInterval );
+
+                $( '.dragon-fire' ).remove();
+                if( false === $( '.content').children().hasClass( 'dragon-final') ){
+                    $( '<img></img>', {
+                        src: 'images/dragon-final.gif',
+                        class: 'dragon-final'
+
+                    } ).fadeIn(1000)
+                        .prependTo( '.content' )
+                        .animate(
+                            {
+                                'position': 'absolute',
+                                'bottom': '300px',
+                                'right': '800px'
+                            }, 10000
+                        )
+                        .fadeOut( 10000 );
+
+                    birdAction.dragonImageEl = document.querySelector( '.dragon-final');
+                    birdAction.dragonImageEl.addEventListener( 'click', birdAction.dragonHitVulnerable );
+                }
+            }
+
+            if ( timeInterval > 100 ){
+                console.log( 'Timeint at completion = ', timeInterval );
+
+                $( '.dragon-final' ).remove();
+                clearInterval( newDragonEntryInterval );
+                return;
+            }
+        }, 1000);
+    },
+    dragonHitStronger: function () {
         var audio = document.getElementById( 'audio' );
-            birdAction.health = document.getElementById( "health" );
+        birdAction.health = document.getElementById( "health" );
         audio.play();
         birdAction.health.value += 5;
         var w, r, imgW, imgR ;
-        imgW = parseInt( window.getComputedStyle( document.querySelector( '.dragon-entry-green' ) ).width);
-        console.log( 'imgWidth= ',imgW );
-        imgR = parseInt( window.getComputedStyle( document.querySelector( '.dragon-entry-green' ) ).right);
+        imgW = parseInt( window.getComputedStyle( birdAction.dragonImageEl ).width);
+        imgR = parseInt( window.getComputedStyle( birdAction.dragonImageEl ).right);
         if( 820 > imgW ){
             w = imgW + 20;
             r = imgR - 20;
-            document.querySelector( '.dragon-entry-green' ).style.width = w + 'px';
-            document.querySelector( '.dragon-entry-green' ).style.right = r + 'px';
+            birdAction.dragonImageEl.style.width = w + 'px';
+            birdAction.dragonImageEl.style.right = r + 'px';
             return;
         }
+    },
+    dragonHitVulnerable: function () {
+        birdAction.health.value -= 5;
+        $( birdAction.dragonImageEl ).fadeOut(100);
+        $( birdAction.dragonImageEl ).fadeIn(100);
     }
 };
 
